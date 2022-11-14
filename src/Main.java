472808189babd1192042a4c85fdccc3d27cf3fe7
Miguel Cortez <mickeycortez2002@gmail.com>
2022-11-13 22:01:45 -0600

    tarea 6

---
+++ src/Main.java
@@ -0,0 +1,63 @@
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+
+public class Main {
+
+    public static void main(String[] args) {
+        prueba1();
+        prueba2();
+        prueba3();
+    }
+
+    private static void prueba1() {
+        File file = new File("prueba1.txt");
+
+        try{
+            FileWriter fileWriter = new FileWriter(file);
+            fileWriter.write("En esta prueba el archivo se crea cada vez que el código se ejecuta");
+            fileWriter.close();
+        }catch (IOException exception){
+            exception.printStackTrace();
+        }
+
+    }
+
+    private static void prueba2() {
+        File file = new File("prueba2.txt");
+
+        try{
+            FileWriter fileWriter = new FileWriter(file, true);
+
+            fileWriter.write("En esta prueba el archivo crece en cada ejecución, lo nuevo se añade al final\n");
+
+            fileWriter.close();
+        }catch (IOException exception){
+            exception.printStackTrace();
+        }
+
+    }
+
+    private static void prueba3() {
+        File file = new File("prueba3.txt");
+        FileWriter fileWriter = null;
+
+        try {
+            fileWriter = new FileWriter(file);
+
+            fileWriter.write("En esta prueba ocurrirá un error\n");
+            fileWriter.write("El resultado fallará: " + (1/0));
+
+            fileWriter.close();
+        }catch (IOException | ArithmeticException exception){
+            exception.printStackTrace();
+            if(fileWriter != null){
+                try{
+                    fileWriter.close();
+                }catch (IOException exception1){
+                    exception.printStackTrace();
+                }
+            }
+        }
+    }
+}
